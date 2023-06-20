3b433a9014ee8ae352162c4b7bbea41a565789a7
Harsh Dehuliya <73236136+harshdehuliya@users.noreply.github.com>
2023-06-20 09:17:43 +0530

    Add files via upload

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"database/sql"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:root@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var testQuery *Queries
+var testDB *sql.DB
+
+func TestMain(m *testing.M) {
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
+
+	if err != nil {
+		panic(err)
+	}
+
+	testQuery = New(testDB)
+
+	os.Exit(m.Run())
+}
